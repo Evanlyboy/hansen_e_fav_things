235918473654ca59a2df02843e705c90a34bf951
Evan <39568306+Evanlyboy@users.noreply.github.com>
2022-10-15 18:39:34 -0400

    added some elements to make things pop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Imports
 import { getData } from "./modules/dataMiner.js";
 
+// This code is built dynamically so more data can be added to the JSON file and it will still work as intended
 (() => {
 
     console.log('main run...ok')
@@ -9,50 +10,41 @@ import { getData } from "./modules/dataMiner.js";
     let cardTemplate = document.querySelector("#cardTemplate").content,
         cardDisplay = document.querySelector('#mainCards');
 
-    // Functions
 
+    // Functions
     function getJSONData (favThings) {
         console.log("getJSONData...ok");
 
+        // Make the thing an object with keys
         let favThingsObj = Object.keys(favThings);
 
+        // Run a loop for each item in the JSON file. We're passing in a variable called objectID that will do things
         favThingsObj.forEach(objectID => {
             // admittedly, this is stupid but it works
+                // target the first child, then target the children
             let panel = cardTemplate.cloneNode(true),
             cardContainer = panel.firstElementChild,
             cardStuff = cardContainer.firstElementChild.children;
+
             // fuckin finally this works
             panel.firstElementChild.id = objectID;
-            
+
+            // make event listener here because they don't work outside of this function
             panel.firstElementChild.addEventListener('click',function(prof) {
                 flipCard(objectID);
             })
-            panel.firstElementChild.addEventListener('mouseover',function() {
-                hoverCard();
-            })
-            console.log(favThings[objectID])
-            console.log(cardStuff)
 
+            // populate our template items with the data from the JSON file in order of appearance
             cardStuff[1].textContent = favThings[objectID].name;
-            cardStuff[2].textContent = favThings[objectID].why;
-            cardStuff[3].textContent = favThings[objectID].desc;
+            cardStuff[3].textContent = favThings[objectID].why;
+            cardStuff[4].textContent = favThings[objectID].desc;
 
             // add the completed element to the page
             cardDisplay.appendChild(panel);
 
             // change the background
             document.querySelector(`#${objectID}`).style.backgroundImage = `url(../../images/${objectID}.jpg)`;
-
-            let htmlIDTag = document.querySelector(`#${objectID}`); // the section tag's contents
-            console.log("this is the htmlIDtag", htmlIDTag)
         })
-
-    //Buttons
-    let buttonCard = document.querySelector(".card");
-    }
-
-    function hoverCard() {
-        // console.log("card hovered upon");
     }
 
     function flipCard(favThing) {
@@ -61,49 +53,41 @@ import { getData } from "./modules/dataMiner.js";
         gsap.to(`#${favThing}`, {y:50, duration: 0.1,})
         gsap.to(`#${favThing}`, {y:0, duration: 0.1, delay:0.1,})
 
-        // We are seeing if there are any classes with cardHide inside our card
+        // We are seeing if there are any classes with cardHide inside our card, and making a removing and adding function
         let cardIdentifier = document.querySelector(`#${favThing}`);
         let classAdder = cardIdentifier.querySelectorAll(".cardHide");
         let classRemover = cardIdentifier.querySelectorAll(".cardInfo");
 
-        // if there is, we must add the info we have to the card
+        // if the length of the classAdder array is more than one, we have bitches to yeet out of there
         if(classAdder.length > 1) {
             console.log("card flipped to true");
-            // console.log("this is the favthing", favThing);
 
             // Data Editing
-
+            // A loop that adds the visible information to each element, and removes the previous one
             classAdder.forEach(classAdder => {
                 classAdder.classList.add('cardInfo');
                 classAdder.classList.remove('cardHide');
             })
 
+            // Then styles the card in the intended way
             cardIdentifier.querySelector('.card').style.backgroundColor = "black";
             cardIdentifier.querySelector('.card').style.opacity = "0.80";
 
-
-            console.log(classAdder);
+        // if the lentgh isn't more than one, it has to be zero, so there are the other kind of bitches to yeet 
         } else {
             console.log("card flipped to false");
 
-            // It's skipping over this because there isn't anything in the ClassAdder
+            // Loop that does the same as above just the inverse
             classRemover.forEach(classRemover => {
                 classRemover.classList.add('cardHide');
                 classRemover.classList.remove('cardInfo');
             })
 
+            // Same as above
             cardIdentifier.querySelector('.card').style.backgroundColor = "transparent";
             cardIdentifier.querySelector('.card').style.opacity = "1";
 
         }
-        
-
-        // bingus = cardData.children,
-        // scrongle = bingus[2,3].classList;
-        // console.log(cardData);
-        // console.log(bingus, scrongle);
-        // On click, we make the background white, and then make a toggle for the hidden classes.
-        
     }
 
     getData(`./data.json`, getJSONData);
